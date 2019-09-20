f8ba1e9dcf0e4a3e81bfd685e12812632be90c37
xjq946 <xjq111006@163.com>
2019-09-21 00:15:49 +0800

    Initial commit

---
+++ provider-ticket/src/main/java/com/example/providerticket/controller/TicketController.java
@@ -0,0 +1,19 @@
+package com.example.providerticket.controller;
+
+import com.example.providerticket.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TicketController {
+
+    @Autowired
+    private TicketService ticketService;
+
+    @GetMapping("/ticket")
+    public String getTicket(){
+        return ticketService.getTicket();
+    }
+}
